ae5edfb13e58bf277c5f808a059bcf255b905e3e
yanbingbin <785175917@qq.com>
2018-09-26 17:41:58 +0800

    去除注释掉的代码

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import Vue from 'vue';
 import analysisDirective from './directives/analysis/directive.js'
 
-//let analysis = {};
-//
-///**
-// * 注册所有指令
-// */
-//analysis.install = function(){
-//  Vue.directive('analysis', analysisDirective);
-//};
-//
-//const addItem = function(value) {
-//	console.log(value)
-//}
-//
-//export {addItem, analysis}
-
 export default {
 	install(Vue, options) {
 		Vue.globalMethod = function () { // 1、添加全局的方法或属性
